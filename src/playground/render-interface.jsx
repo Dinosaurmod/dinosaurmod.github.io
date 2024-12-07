84665daf833897a5f70f16a5f9de25ee94c1443f
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-12-07 18:32:07 +0100

    WHY WONT YOU WOOOOOROORKORKEUJ

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -512,7 +512,7 @@ Interface.propTypes = {
         remixId: PropTypes.string,
         tooLarge: PropTypes.bool,
         author: PropTypes.string,
-        releaseDate: PropTypes.shape(new Date),
+        releaseDate: PropTypes.shape(Date),
         isUpdated: PropTypes.bool
     }),
     remixedProjectInfo: PropTypes.shape({
